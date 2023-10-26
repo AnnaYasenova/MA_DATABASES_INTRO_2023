f150301b79cdbb8ff9700c2ec79534b498606c5d
LeusMaximus <leus.maximus@gmail.com>
2023-10-27 01:50:24 +0300

    Added solution 2

--- HW1/HW1_MaksymLeonidov.sql
+++ HW1/HW1_MaksymLeonidov.sql
@@ -7,6 +7,11 @@ SELECT * FROM customer;
    2. З таблиці customer виведіть лише ім’я, прізвище та електронну пошту.
       В результуючій таблиці колонки мають називатися "First Name", "Last Name", "Email".
 */
+SELECT
+    first_name AS "First Name",
+    last_name AS "Last Name",
+    email AS "Email"
+FROM customer;
 
 /*
    3. З таблиці address виведіть колонки address, district, postal_code.
