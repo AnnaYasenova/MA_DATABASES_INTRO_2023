09b2e43c64f977ca357f084392f49b1c36daa9a0
olgadobrodii <dobrodey.olga@gmail.com>
2023-11-27 16:09:30 +0200

    Hometask 1 Test update

--- HW1/HW1_OlhaDobrodii.sql
+++ HW1/HW1_OlhaDobrodii.sql
@@ -1,49 +1,58 @@
 use sakila;
-
+-- 1
 select * from sakila.customer;
 
+-- 2
 select 
 first_name as "First_Name",
 last_name as "Last_Name",
 email as "Email"
 from customer;
 
-select Address, District, Postal_code from address;
+-- 3
+select 
+address as "Address",
+district as "District",
+postal_code as "Postal_code"
+from address
+order by address, district desc;
 
-select District from address
-order by District asc;
-
-select Address from address
-order by Address desc;
-
+-- 4
 select title, rental_rate from film
 where rental_rate > 3;
 
+-- 5
 select title, description, rating from film
 where rating in ("G", "PG", "R");
 
+-- 6
 select * from film_text
 where description like '%database%';
 
+-- 7
 select * from film
-where rental_rate = 3 and replacement_cost < 12;
+where rental_duration = 3 and replacement_cost < 12;
 
+-- 8
 select * from film
 where rating = 'G' and replacement_cost > 15;
 
+-- 9
 select * from film
-where length <= 90 and length  >= 60;
+where length between 60 and 90;
 
+-- 10
 select * from film
-where length > 90 or length  < 60;
+where length < 60 or length  > 90;
 
+-- 11
 select title from film
 where rental_duration = 6 or 7
 and rental_rate >= 4 
-and special_features 
-and special_features like '%Trailers%' 
-and special_features like '%Commentaries%';
+and (special_features like '%Trailers%' 
+or special_features like '%Commentaries%');
 
+-- 12
 select * from film
-where rating = 'G' and length  < 60
-or rating = 'R' and special_features like '%Commentaries%';
+where (rating = 'G' and length < 60)
+or (rating = 'R' and special_features like '%Commentaries%');
